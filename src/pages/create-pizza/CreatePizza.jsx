cbf9221eaf90ff28561380b9965b3e8dfd6c2d85
Uulkelsin Bektemirova <100680512+ukatls@users.noreply.github.com>
2022-04-09 15:51:07 +0600

    editing CreatePizza

--- src/pages/create-pizza/CreatePizza.jsx
+++ src/pages/create-pizza/CreatePizza.jsx
@@ -1,17 +1,21 @@
-import React from 'react'
+import css from './CreatePizza.module.css'
 
 export default function CreatePizza() {
   return (
     <div className='container'>
+      <div className={css.createPizza}>
         <h1>Create New Item</h1>
         <h3>Image</h3>
-        <img src="" alt="#" />
+        <div className={css.imgWrapper}>
+          <img className={css.img} src="https://cdn-icons.flaticon.com/png/512/4718/premium/4718232.png?token=exp=1649497092~hmac=ab1bfb2868ffb714f3e15664a2f75286" alt="#" />
+        </div>
         <h3>Name</h3>
-        <input type="text" name="" id="" />
+        <input className={css.input} type="text" placeholder='Pizza' />
         <h3>Price</h3>
-        <input type="text" name="" id="" />
+        <input className={css.input} type="text" placeholder='$ 6' />
         <h3>Description</h3>
-        <textarea name="" id="" cols="30" rows="10"></textarea>
+        <textarea className={css.input} cols="30" rows="10"></textarea>
+      </div>
     </div>
   )
 }
